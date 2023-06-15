acc7864d2cc6b27921611cf3cce137ccd1a60695
kashifalidevstudio <kashifali@devstudio.us>
2023-06-15 10:52:18 +0500

    fontawesome and toastr installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular_CRUD_Using_Routes_APP';
+export class AppComponent implements OnInit{
+    title = 'Angular_CRUD_Using_Routes_APP';
+
+    constructor(private _toastr: ToastrService) {
+    }
+
+    ngOnInit(): void {
+        this._toastr.success("User Added", "User Registration");
+        this._toastr.info("User Added", "User Registration");
+        this._toastr.error("User Added", "User Registration");
+    }
+
 }
