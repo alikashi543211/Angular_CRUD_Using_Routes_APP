d09676e88ba852b050e80babdc3553ddeee1db0d
kashifalidevstudio <kashifali@devstudio.us>
2023-06-15 11:01:28 +0500

    sweetalert2 installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
     title = 'Angular_CRUD_Using_Routes_APP';
 
     constructor(private _toastr: ToastrService) {
     }
 
     ngOnInit(): void {
-        this._toastr.success("User Added", "User Registration");
-        this._toastr.info("User Added", "User Registration");
-        this._toastr.error("User Added", "User Registration");
+
     }
 
 }
